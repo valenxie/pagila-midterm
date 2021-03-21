cc56370ebdc7ad1b62cf32ebb0747226d1d70433
Valen <valenthetallest@yahoo.com>
2021-03-21 03:42:47 -0700

    fixed typo

--- sql/05.sql
+++ sql/05.sql
@@ -4,11 +4,11 @@
  */
 
 CREATE TABLE project (
-	created_at TIMESTAMPTZ, 
-	updated_at TIMESTAMPTZ, 
-	author_id BIGINT NOT NULL, 
+    created_at TIMESTAMPTZ, 
+    updated_at TIMESTAMPTZ, 
+    author_id BIGINT NOT NULL, 
     id SERIAL PRIMARY KEY, 
-	target_id INTEGER NOT NULL, 
+    target_id INTEGER NOT NULL, 
 	project_id INTEGER NOT NULL UNIQUE, 
 	action SMALLINT NOT NULL, 
 	target_type VARCHAR(2) NOT NULL,     
